00fedcd6e1ab36ea17deac556645e8bd68e6eedc
alexnadeev <anadeev83@gmail.com>
2019-10-09 12:01:10 -0500

    "test_commit"

---
+++ hanoi_tower.py
@@ -0,0 +1,9 @@
+def tower(n, start = "1", dest = "2", spare = "3"):
+    if n == 1:
+        print(f"from {start} move to {dest}")
+    else:
+        tower(n-1, start, spare, dest)
+        tower(1) #print(f"from {start} move to {dest}")
+        tower(n-1, spare, dest, start)
+
+tower(3)
